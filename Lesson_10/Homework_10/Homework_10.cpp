51093a7796ca41b22f2f8dac422207a4b15a00fa
Horychenko Maksym <horychenkomax@gmail.com>
2025-02-14 10:34:40 +0200

    Implement task 3

--- Lesson_10/Homework_10/Homework_10.cpp
+++ Lesson_10/Homework_10/Homework_10.cpp
@@ -31,13 +31,29 @@ bool find(const int* arr, int size, int elem) {
 void task_2() {
     int arr[5] = {1,2,3,4,5};
     int number = 2;
-    std::cout << std::boolalpha << find(arr, 5, number);
+    std::cout << std::boolalpha << find(arr, 5, number) << std::endl;
+}
+
+bool calculateSum(const double* arr, int arrSize, double& sum) {
+    if (arrSize <= 0) return false;
+    for (int i = 0; i < arrSize; i++) {
+        sum += arr[i];
+    }
+    return true;
+}
+
+void task_3() {
+    double arr[5] = { 1,2.6,3,4,5 };
+    double sum = 0;
+    std::cout << std::boolalpha << calculateSum(arr, 5, sum) << std::endl;
+    std::cout << sum;
 }
 
 int main()
 {
     task_1();
     task_2();
+    task_3();
 
 }
 
